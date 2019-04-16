57669178701a81a72d2c3e2da07874b871bc2102
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:33:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,5 @@
 class Hash
   def keys_of(*arguments)
-    collect do |key, value|
-      (arguments.include?(value) ? key : nil).compact
-    end
+    collect { |key, value| arguments.include?(value) ? key : nil}.compact
   end
 end
